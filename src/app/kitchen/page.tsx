0981b46c4046af5233eee715b2e903c4e45baeba
Baza-812 <v.sp77@tutanota.com>
2025-11-06 23:57:51 +0300

    Update page.tsx

--- src/app/kitchen/page.tsx
+++ src/app/kitchen/page.tsx
@@ -8,7 +8,9 @@ export const fetchCache = 'force-no-store';
 type SP = { [key: string]: string | string[] | undefined };
 
 export default function Page({ searchParams }: { searchParams: SP }) {
-  const keyParam = (Array.isArray(searchParams.key) ? searchParams.key[0] : searchParams.key) || 'kitchen_o555';
+  const keyParam =
+    (Array.isArray(searchParams.key) ? searchParams.key[0] : searchParams.key) ||
+    'kitchen_o555';
 
   return (
     <Suspense fallback={<div className="p-6">Загрузка…</div>}>
